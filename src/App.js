e0c6595315ae3914fd1eeae6995f99f642f71d27
zinakalu <zina.kalu@gmail.com>
2023-07-20 21:21:40 -0400

    Co-authored-by: Carmelle Millar <cmillar-titan@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import { MapContainer, TileLayer, Polygon, Tooltip } from "react-leaflet";
+import { useEffect } from "react";
+
+import {
+  MapContainer,
+  TileLayer,
+  Polygon,
+  Tooltip,
+  Popup,
+  Marker,
+} from "react-leaflet";
 import "./App.css";
 import { statesData } from "./data";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
+
+/**
+ * https://stackoverflow.com/questions/65427633/fetch-data-from-local-api
+
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+https://react-leaflet.js.org/docs/api-map/
+ */
 
 function App() {
   const center = [40.63463151377654, -97.89969605983609];
   // const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("hi?");
+    const url = "/activities";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/activities");
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
@@ -22,46 +56,47 @@ function App() {
           const coordinates = state.geometry.coordinates[0];
           const stateName = state.properties.name;
 
-          return(
+          return (
             <Polygon
-            key={stateName}
-            pathOptions={{
-              fillColor: '#FD8D3C',
-              fillOpacity: 0.7,
-              weight: 2,
-              opacity: 1,
-              dashArray: 3,
-              color: 'white',
-            }}
-            positions={coordinates}
-            eventHandlers={{
-              mouseover: (e) => {
-                const layer = e.target;
-                layer.setStyle({
-                  fillOpacity: 0.7,
-                  weight: 2,
-                  dashArray: '',
-                  color: '#666',
-                  fillColor: '#D45962',
-                });
-              },
-              mouseout: (e) => {
-                const layer = e.target;
-                layer.setStyle({
-                  fillOpacity: 1,
-                  weight: 2,
-                  dashArray: '3',
-                  color: 'white',
-                  fillColor: '#FD8D3C',
-                });
-              },
-              click: () => {
-                // navigate(`/park-details/${state.properties.name}`);
-              },
-            }}
-          >
-            <Tooltip>{stateName}</Tooltip>
-          </Polygon>
+              key={stateName}
+              pathOptions={{
+                fillColor: "#FD8D3C",
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: 3,
+                color: "white",
+              }}
+              positions={coordinates}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 2,
+                    dashArray: "",
+                    color: "#666",
+                    fillColor: "#D45962",
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 1,
+                    weight: 2,
+                    dashArray: "3",
+                    color: "white",
+                    fillColor: "#FD8D3C",
+                  });
+                },
+                click: () => {
+                  // navigate(`/park-details/${state.properties.name}`);
+                },
+              }}
+            >
+              <Popup>{stateName}</Popup>
+              <Tooltip>{stateName}</Tooltip>
+            </Polygon>
           );
         })}
       </MapContainer>
