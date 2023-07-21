1c9cee19005faa10e0eb2c3e6c39736b74db34f8
zinakalu <zina.kalu@gmail.com>
2023-07-20 20:37:29 -0400

    installed react leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { MapContainer, TileLayer, Polygon, Tooltip } from 'react-leaflet';
 import './App.css';
+import { statesData } from './data'
 
 function App() {
+  const center = [40.63463151377654, -97.89969605983609];
+  // const navigate = useNavigate();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MapContainer center={center} zoom={10} style={{ width: '100vw', height: '100vh' }}>
+      <TileLayer
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=NXYqFx4vIix4Ixh8KlN6"
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      />
+      {statesData.features.map((state) => {
+        const coordinates = state.geometry.coordinates[0];
+        const stateName = state.properties.name;
+
+        return (
+        <></>
+        );
+        })}
+    </MapContainer>
     </div>
   );
 }
